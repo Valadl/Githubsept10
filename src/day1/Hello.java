d524569fcdde959dc4e25c5ebbf9ad57dd2138c7
Speedy777 <admin@primemotorcarrier.com>
2022-09-10 15:40:22 -0500

    ManuOne

--- src/day1/Hello.java
+++ src/day1/Hello.java
@@ -1,6 +1,9 @@
 package day1;
 
 public class Hello {
-
+    public static void main(String[] args) {
+        System.out.println("Hellooo");
+        System.out.println("Manu");
+    }
     //test
 }
