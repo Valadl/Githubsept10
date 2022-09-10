d261dd983844f9a12650b82ff283dc54b346b0d5
Speedy777 <admin@primemotorcarrier.com>
2022-09-10 15:41:58 -0500

    ManuTwo

--- src/day1/Hello.java
+++ src/day1/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hellooo");
         System.out.println("Manu");
+        System.out.println("2222");
     }
     //test
 }
