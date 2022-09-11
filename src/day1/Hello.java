1ade978aff44f1ce767c0695a7e955d58175328a
Speedy777 <admin@primemotorcarrier.com>
2022-09-11 12:36:11 -0400

    ///"r" Test01

--- src/day1/Hello.java
+++ src/day1/Hello.java
@@ -2,9 +2,7 @@ package day1;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hellooo");
-        System.out.println("Manu");
-        System.out.println("2222");
+        System.out.printf("r");
     }
     //test
 }
